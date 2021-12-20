96e2c0ff6a92285616b477add146d143a0e56298
Wartimer <danryzhkov@mail.ru>
2021-12-20 20:44:55 +0300

    Пофиксил PlayerView

--- Assets/Scripts/Player/PlayerView.cs
+++ Assets/Scripts/Player/PlayerView.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 namespace TeamBlue_Asteroids
 {
-    internal sealed class PlayerView : MonoBehaviour
+    internal sealed class PlayerView : MonoBehaviour, IDisposable, IDamagable
     {
+        [SerializeField] private PlayerModel _playerModel;
+        internal event Action PlayerDestroyed;
+        private UnitStats _stats = new UnitStats();
+        
         private AudioSource _audioSource;
         private AudioClip _shootSound;
 
@@ -14,6 +18,31 @@ namespace TeamBlue_Asteroids
             set => _shootSound = value;
         }
 
+        private void OnEnable()
+        {
+            PlayerInit();
+        }
+        
+        private void PlayerInit()
+        {
+            _stats.Health = _playerModel.HitPoints;
+            _stats.Armour = _playerModel.Armour;
+        }
+        
+        public void TakeDamage(int damage)
+        {
+            if (_stats.Health > 0)
+                _stats.Health -= damage;
+            if (_stats.Health <= 0)
+                Dispose();
+        }
+        
+        public void Dispose()
+        {
+            PlayerDestroyed?.Invoke();
+            Destroy(gameObject);
+        }
+        
         private void Awake()
         {
             _audioSource = GetComponent<AudioSource>();
