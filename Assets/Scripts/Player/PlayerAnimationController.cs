2d40b56299d189b3ca892c80843a965163cc3cee
Wartimer <danryzhkov@mail.ru>
2021-12-26 13:35:41 +0300

    Небольшие фиксы

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 namespace TeamBlue_Asteroids
 {
-    internal sealed class PlayerAnimationController : IFixedExecute
+    internal sealed class PlayerAnimationController : IFixedExecute, ICleanup
     {
+        public event Action<IController> PlayerRemoved;
         
         private PlayerView _player;
-        private Animator _animator;              
+        private Animator _animator;
+        
+        private float _horizontal;
+        private IUserInputProxy _horizontalInputProxy;
+        private static readonly int Turn = Animator.StringToHash("Turn");
 
-        public PlayerAnimationController(PlayerView player)
+        public PlayerAnimationController(IUserInputProxy horizontalInputProxy, PlayerView player)
         {          
             _player = player;  
             _animator = _player.gameObject.GetComponent<Animator>();
+            _horizontalInputProxy = horizontalInputProxy;
+            
+            _horizontalInputProxy.AxisChange += OnHorizontalAxisChange;
+            _player.PlayerDestroyed += Cleanup;
         }
 
-
+        private void OnHorizontalAxisChange(float value)
+        {
+            _horizontal = value * 0.4f;
+            Debug.Log(_horizontal);
+        }
+        
         public void FixedExecute(float deltaTime)
         {
-            if (Input.GetKey(KeyCode.A))
+            
+            _animator.SetFloat(Turn, _horizontal);
+            /*if (Input.GetKey(KeyCode.A))
             {
                 _animator.SetFloat("Turn", -1);
             }
@@ -32,10 +48,15 @@ namespace TeamBlue_Asteroids
             if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
             {
                 _animator.SetFloat("Turn", 0);
-            }
+            }*/
         }
 
-
+        public void Cleanup()
+        {
+            _player.PlayerDestroyed -= Cleanup;
+            _horizontalInputProxy.AxisChange -= OnHorizontalAxisChange;
+            PlayerRemoved?.Invoke(this);
+        }
 
     }
 }
